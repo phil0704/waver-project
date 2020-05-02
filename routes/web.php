dbcc01d1250e8531882832a46bb411fc8ca4ecab
Philip Querimit <philip@Philips-MacBook-Air.local>
2020-05-02 15:30:09 -0600

    updated seeds

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
